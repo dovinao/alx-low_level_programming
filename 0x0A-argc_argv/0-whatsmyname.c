e4d82c74ca742907794fc226721f70f2c9377b23
dovinao <lovinaonyinye123#gmail.com>
2023-05-15 08:43:06 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main- prints its name
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return:Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+A readme file for ALX task 0x0A. C - argc, argv
